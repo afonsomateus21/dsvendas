927ec85a19375c4285573c9aca66579fba8e7aac
Afonso Mateus <afonsomateus@zeke.local>
2021-05-09 19:23:45 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { SellerDTO } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number
+    amount: number;
+    date: string;
+    sellerDTO: SellerDTO;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
---
+++ frontend/src/types/seller.ts
@@ -0,0 +1,5 @@
+export type SellerDTO = {
+    id: number;
+    name: string;
+}
+
